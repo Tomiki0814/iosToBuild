d9ff386a03e90e0f79eacbe70cd675ae8346667e
Aiteam55 <mitiaandria1@gmail.com>
2023-08-28 20:06:10 +0300

    add capacitor

---
+++ project/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tptmbds.egouvernance',
+  appName: 'e-gouvernance',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
